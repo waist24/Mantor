5b0f5c2ea16f77d0e0e4baa1617490344326b0e9
waist24 <waist24@gmail.com>
2016-06-22 21:27:33 +0900

    Git Test

---
+++ Mentor.MessagingServer/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mentor.MessagingServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
